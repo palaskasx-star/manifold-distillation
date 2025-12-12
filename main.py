dfb1917b349f8cd53fbf3f46f4bf3bef16d70d60
palaskasc <palaskasx@gmail.com>
2025-12-12 10:50:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -189,7 +189,7 @@ def get_args_parser():
     parser.add_argument('--world_size', default=1, type=int,
                         help='number of distributed processes')
     parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
-    parser.add_argument('--local_rank', default=0, type=int)
+    parser.add_argument('--local-rank', '--local_rank', type=int, default=0, help='local rank for DistributedDataParallel')
     return parser
 
 
@@ -472,6 +472,7 @@ if __name__ == '__main__':
     if args.output_dir:
         Path(args.output_dir).mkdir(parents=True, exist_ok=True)
     main(args)
+
 
 
 
